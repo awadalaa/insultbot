4b4f232c1f6cfd6e152121718fc7bd159a64f436
Alaa Awad <alaa.awad.mail@gmail.com>
2016-05-12 22:46:31 -0400

    config file to store twitter credentials

---
+++ config.js
@@ -0,0 +1,8 @@
+// register for a twitter account and go to 
+// https://dev.twitter.com/apps/new to get these:
+module.exports = {
+  consumer_key:         'set_this',
+  consumer_secret:      'set_this',
+  access_token:         'set_this',
+  access_token_secret:  'set_this'
+}
