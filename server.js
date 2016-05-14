d4f95389c994cccfd926d3ec7bdb60d5f84ba80d
Alaa Awad <alaa.awad.mail@gmail.com>
2016-05-13 23:11:07 -0400

    env var port for heroku

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('App listening on port 3000!');
 });
