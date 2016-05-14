c6ca6d7b5eaa656341f2bc31f35fe0edcc2772a7
Alaa Awad <alaa.awad.mail@gmail.com>
2016-05-13 23:02:54 -0400

    adding web server

--- server.js
+++ server.js
@@ -2,4 +2,14 @@ var express = require('express');
 var app = express();
 var twitterbot = require('./twitterbot');
 
+console.log('app starting...');
 twitterbot.init();
+
+console.log('server starting...');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+});
